2a2dc49e909d44d1264c4a72705a2a0c02117519
Alexander Jamshidi <alexanderjamshidi@Alexanders-Air.localdomain>
2021-12-09 16:16:48 +0000

    Added attribute reader and dock_bike

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,7 +1,23 @@
-class DockingStation 
+class DockingStation
+ 
   def release_bike
     Bike.new
   end
+  
+  def dock_bike(bike_name)
+    @bike_name = bike_name
+  end
+
+attr_reader :bike_name
+
+  def is_there_bike?
+    if self.bike_name != ""
+      self.bike_name
+    else
+      nil
+    end
+  end
+
 end
 
 
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,6 +1,20 @@
 require 'docking_station'
 describe DockingStation do
+    station = DockingStation.new
+    green_bike = Bike.new
+
     it {expect(DockingStation.new).to respond_to :release_bike} 
     it {expect(DockingStation.new.release_bike.working?).to be true}
+    it {expect(DockingStation.new).to respond_to :dock_bike}
+
     
+    it {expect(DockingStation.new.dock_bike(green_bike)).to eq green_bike }
+
+    it { expect(station.is_there_bike?).to eq nil}
+
+    it "checks if station has any bikes" do
+        station.dock_bike(green_bike)
+        expect(station.is_there_bike?).to eq green_bike
+    end 
+
 end
